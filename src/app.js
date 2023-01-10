d67efff478e24aa4790777ca1c354aabd1eff101
NeoClean <salado2012@gmail.com>
2023-01-10 13:31:48 +0000

    Excusillas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excusa").innerHTML = generateExcuse();
+  });
 };
+// EXCUSAS
+let generateExcuse = () => {
+  let who = ["El perro", "Mi hermana", "Su conejo", "Mi cocodrilo"];
+  let action = ["comió", "cedio", "aplasto", "rompio"];
+  let what = ["mi tarea", "las llaves", "el auto"];
+  let when = [
+    "despues de clases.",
+    "justo a tiempo.",
+    "cuando terminé.",
+    "durante mi cena.",
+    "mientras estaba en el baño."
+  ];
+
+  let getRandomInt = Math.floor(Math.random() * who.length);
+  let getRandomAct = Math.floor(Math.random() * action.length);
+  let getRandomWhat = Math.floor(Math.random() * what.length);
+  let getRandomWhen = Math.floor(Math.random() * what.length);
+
+  return (
+    who[getRandomInt] +
+    " " +
+    action[getRandomAct] +
+    " " +
+    what[getRandomWhat] +
+    " " +
+    when[getRandomWhen]
+  );
+};
+
+// Created by NeoClean
